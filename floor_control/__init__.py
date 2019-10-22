beec384d2e7f0a894d12414adc28d69c99249a51
Tom Gurion <nagasaki45@gmail.com>
2019-10-22 12:10:15 +0100

    Some naming and defaults improvements

--- floor_control/__init__.py
+++ floor_control/__init__.py
@@ -3,19 +3,19 @@ import audioop
 from . import core
 
 
-class RmsFilterDetector:
+class FloorControlDetector:
     def __init__(
         self,
         sample_rate,
         sample_width,
-        buffer_size,
-        cutoff_freq,
-        hysteresis,
+        buffer_duration=0.02,
+        cutoff_freq=0.35,
+        hysteresis=0.1,
         num_of_interactants=2,
     ):
         self._sample_width = sample_width
         self._filters = [
-            core.Filter(cutoff_freq=cutoff_freq, sample_rate=sample_rate / buffer_size)
+            core.Filter(cutoff_freq=cutoff_freq, sample_rate=1 / buffer_duration)
             for _ in range(num_of_interactants)
         ]
         self._argmax = core.StableArgmax(hysteresis=hysteresis)
