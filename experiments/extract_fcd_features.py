Correct FCD threshold calculation to be relative

experiments/extract_fcd_features.py
@@ -1,4 +1,3 @@
-import audioop
 import pathlib
 
 import numpy as np
@@ -11,33 +10,26 @@ OUT_DIR = pathlib.Path('features') / 'FCD'
 BUFFER_DURATION = 0.02
 
 
-class RmsExtractor():
-    def __init__(self, sample_width, **_):
-        self._sample_width = sample_width
-
-    def process(self, fragments):
-        return [audioop.rms(x, self._sample_width) for x in fragments]
-
-
 def main():
     utils.path.empty_dir(OUT_DIR)
 
     data_gen = utils.duel.load_sessions_gen()
 
     for session in data_gen:
+        samples, sample_rate = utils.duel.load_samples(session)
+
         for part in session['parts']:
+            start_pos = int(part['start_time'] * sample_rate)
+            end_pos = int(part['end_time'] * sample_rate)
+            buffer_size = int(sample_rate * BUFFER_DURATION)
+
+            part_samples = samples[start_pos:end_pos]
+
             out_filepath = OUT_DIR / f'{session["name"]}-{part["name"]}.npy'
             print(f'Generating {out_filepath}')
 
-            gen = utils.audio.wav_per_buffer_feature_extractor_gen(
-                session['audio_filepath'],
-                part['start_time'],
-                part['end_time'],
-                BUFFER_DURATION,
-                session['swapped_stereo'],
-                extractor_class=RmsExtractor,
-            )
-            rms = np.array(list(gen))
+            buffers = utils.audio.to_chunks(part_samples, buffer_size)
+            rms = np.apply_along_axis(utils.audio.rms, axis=1, arr=buffers)
 
             np.save(out_filepath, rms)
 

experiments/extract_lstm_features.py
@@ -2,24 +2,13 @@ import pathlib
 
 import numpy as np
 import scipy.stats as sstats
-import scipy.signal as ssignal
 
+import utils.audio
 import utils.duel
 import utils.path
-import utils.yin
 
 OUT_DIR = pathlib.Path('features') / 'LSTM'
 BUFFER_DURATION = 0.05
-
-
-def to_chunks(samples, chunk_size):
-    samples = np.concatenate(
-        (
-            samples,
-            np.zeros(shape=(chunk_size - len(samples) % chunk_size, samples.shape[-1])),
-        )
-    )
-    return samples.reshape(-1, chunk_size, samples.shape[-1])
 
 
 def calculate_voice_activity(textgrid, start_time, end_time, buffer_duration):
@@ -29,40 +18,6 @@ def calculate_voice_activity(textgrid, start_time, end_time, buffer_duration):
         [bool(utts.get_annotations_by_time(t)) for utts in [a_utts, b_utts]]
         for t in np.arange(start_time, end_time, buffer_duration)
     ])
-
-
-def calculate_freq(buffers, sample_rate):
-    return np.apply_along_axis(
-        utils.yin.compute_yin,
-        axis=1,
-        arr=buffers,
-        sample_rate=sample_rate
-    )
-
-
-def rms(values):
-    return np.sqrt(np.mean(np.square(values)))
-
-
-def calculate_power(buffers):
-    return 10 * np.log10(np.apply_along_axis(rms, axis=1, arr=buffers))
-
-
-def calculate_spectral_flux(buffers):
-    n_buffers, buffer_size, channels = buffers.shape
-    flatten = buffers.reshape(n_buffers * buffer_size, -1)
-    _, _, stft = ssignal.stft(
-        flatten,
-        nperseg=buffer_size,
-        axis=0,
-        window='hamming',
-        noverlap=0,
-    )
-    abs_stft = np.abs(stft)
-    diff = abs_stft[:, :, :-1] - abs_stft[:, :, 1:]
-    rectified = (diff + np.abs(diff)) / 2
-    flux = np.sum(rectified, axis=0) / np.sum(abs_stft[:, :, :-1], axis=0)
-    return flux.T
 
 
 def calculate_X_and_y(
@@ -76,16 +31,16 @@ def calculate_X_and_y(
 
     samples = samples[int(start_time * sample_rate):int(end_time * sample_rate)]
     buffer_size = int(sample_rate * buffer_duration)
-    buffers = to_chunks(samples, buffer_size)
+    buffers = utils.audio.to_chunks(samples, buffer_size)
 
     y = calculate_voice_activity(textgrid, start_time, end_time, buffer_duration)
 
-    freq = calculate_freq(buffers, sample_rate)
+    freq = utils.audio.calculate_freq(buffers, sample_rate)
     pitch = 69 + 12 * np.log2(freq / 440)  # MIDI note
     pitch[pitch < 0] = 0
     voiced = (freq != 0)
-    power = np.clip(calculate_power(buffers), -96, 0)  # 96dB is 16bit dynamic range
-    spectral_flux = np.nan_to_num(calculate_spectral_flux(buffers))
+    power = np.clip(utils.audio.calculate_power(buffers), -96, 0)  # 96dB is 16bit dynamic range
+    spectral_flux = np.nan_to_num(utils.audio.calculate_spectral_flux(buffers))
 
     X = np.hstack([
         y,

experiments/predict_vad.py
@@ -1,6 +1,8 @@
+import audioop
 import pathlib
 import subprocess
 import tempfile
+import wave
 
 import numpy as np
 import webrtcvad
@@ -23,25 +25,6 @@ def upsample(source, target):
     ])
 
 
-class VadDetector:
-    def __init__(
-        self,
-        sample_rate,
-        vad_mode,
-        **_,
-    ):
-        self._vad = webrtcvad.Vad(vad_mode)
-        self._sample_rate = sample_rate
-        self._current_floor_holder = None
-
-    def process(self, buffer):
-        vad_vals = [self._vad.is_speech(x, self._sample_rate) for x in buffer]
-        # Change floor holder when only one is vocalising
-        if sum(vad_vals) == 1:
-            self._current_floor_holder = vad_vals.index(True)
-        return self._current_floor_holder
-
-
 def main():
     utils.path.empty_dir(OUT_DIR)
 
@@ -58,16 +41,41 @@ def main():
                 out_filepath = OUT_DIR / f'{session["name"]}-{part["name"]}.npy'
                 print(f'Generating {out_filepath}')
 
-                gen = utils.audio.wav_per_buffer_feature_extractor_gen(
-                    tf.name,
-                    part['start_time'],
-                    part['end_time'],
-                    BUFFER_DURATION,
-                    session['swapped_stereo'],
-                    extractor_class=VadDetector,
-                    extractor_params={'vad_mode': 3},
-                )
-                np.save(out_filepath, np.array(list(gen)).astype(float))
+                with wave.open(tf.name) as f:
+                    sample_rate = f.getframerate()
+                    sample_width = f.getsampwidth()
+                    buffer_size = int(sample_rate * BUFFER_DURATION)
+                    channels = f.getnchannels()
+
+                    vad = webrtcvad.Vad(3)  # aggressive vad mode
+
+                    pos = 0  # Counting samples, not bytes
+                    start_pos = int(sample_rate * part['start_time'])
+                    end_pos = int(sample_rate * part['end_time'])
+
+                    # Seek to start_time
+                    f.readframes(start_pos)
+                    pos += start_pos
+
+                    results = []
+                    while pos < end_pos:
+                        buffer = f.readframes(buffer_size)
+                        if len(buffer) != buffer_size * sample_width * channels:
+                            break
+                        l = audioop.tomono(buffer, sample_width, 1, 0)
+                        r = audioop.tomono(buffer, sample_width, 0, 1)
+                        if session['swapped_stereo']:
+                            l, r = r, l
+                        vad_vals = [vad.is_speech(x, sample_rate) for x in [l, r]]
+                        # Change floor holder when only one is vocalising
+                        if sum(vad_vals) == 1:
+                            current_floor_holder = vad_vals.index(True)
+                        else:
+                            current_floor_holder = results[-1]
+                        results.append(current_floor_holder)
+                        pos += buffer_size
+
+                np.save(out_filepath, np.array(results).astype(float))
 
 
 if __name__ == '__main__':

experiments/utils/audio.py
@@ -1,48 +1,48 @@
-import audioop
-import wave
+import numpy as np
+import scipy.signal as ssignal
+
+from . import yin
 
 
-def wav_per_buffer_feature_extractor_gen(
-    filepath,
-    start_time,
-    end_time,
-    buffer_duration,
-    swap_stereo,
-    extractor_class,
-    extractor_params=None,
-):
-    '''
-    Stream a wav file through a feature extractor.
-    '''
-    if extractor_params is None:
-        extractor_params = {}
+def to_chunks(samples, chunk_size):
+    samples = np.concatenate(
+        (
+            samples,
+            np.zeros(shape=(chunk_size - len(samples) % chunk_size, samples.shape[-1])),
+        )
+    )
+    return samples.reshape(-1, chunk_size, samples.shape[-1])
 
-    with wave.open(str(filepath)) as f:
-        sample_rate = f.getframerate()
-        sample_width = f.getsampwidth()
-        buffer_size = int(sample_rate * buffer_duration)
-        channels = f.getnchannels()
 
-        extractor_params['sample_rate'] = sample_rate
-        extractor_params['sample_width'] = sample_width
+def calculate_freq(buffers, sample_rate):
+    return np.apply_along_axis(
+        yin.compute_yin,
+        axis=1,
+        arr=buffers,
+        sample_rate=sample_rate
+    )
 
-        extractor = extractor_class(**extractor_params)
 
-        pos = 0  # Counting samples, not bytes
-        start_pos = int(sample_rate * start_time)
-        end_pos = int(sample_rate * end_time)
+def rms(values):
+    return np.sqrt(np.mean(np.square(values)))
 
-        # Seek to start_time
-        f.readframes(start_pos)
-        pos += start_pos
 
-        while pos < end_pos:
-            buffer = f.readframes(buffer_size)
-            if len(buffer) != buffer_size * sample_width * channels:
-                break
-            l = audioop.tomono(buffer, sample_width, 1, 0)
-            r = audioop.tomono(buffer, sample_width, 0, 1)
-            if swap_stereo:
-                l, r = r, l
-            yield extractor.process([l, r])
-            pos += buffer_size
+def calculate_power(buffers):
+    return 10 * np.log10(np.apply_along_axis(rms, axis=1, arr=buffers))
+
+
+def calculate_spectral_flux(buffers):
+    n_buffers, buffer_size, channels = buffers.shape
+    flatten = buffers.reshape(n_buffers * buffer_size, -1)
+    _, _, stft = ssignal.stft(
+        flatten,
+        nperseg=buffer_size,
+        axis=0,
+        window='hamming',
+        noverlap=0,
+    )
+    abs_stft = np.abs(stft)
+    diff = abs_stft[:, :, :-1] - abs_stft[:, :, 1:]
+    rectified = (diff + np.abs(diff)) / 2
+    flux = np.sum(rectified, axis=0) / np.sum(abs_stft[:, :, :-1], axis=0)
+    return flux.T

floor_control/core.py
@@ -13,7 +13,7 @@ class StableArgmax:
         argsort = np.argsort(samples)
         max_ = samples[argsort[-1]]
         next_ = samples[argsort[-2]]
-        if max_ - next_ > self._hysteresis:
+        if next_ and max_ / next_ > (1 + self._hysteresis):
             self._previous = argsort[-1]
         return self._previous
 